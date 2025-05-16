04634b8a78bbf8347397338b182380de7c4f5436
DESKTOP-560PLBA <desktop-560plba@epn.edu.ec>
2025-05-16 15:22:18 -0500

    Second version

---
+++ app/src/main/java/com/sebastian/guerra/cazarpatos/LoginActivity.kt
@@ -0,0 +1,30 @@
+package com.sebastian.guerra.cazarpatos
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var buttonLogin: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        buttonLogin = findViewById(R.id.buttonLogin)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        buttonLogin.setOnClickListener {
+            Toast.makeText(this, getString(R.string.buttonText_LoginClicked), Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
